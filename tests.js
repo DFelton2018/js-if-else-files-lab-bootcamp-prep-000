566aa8c4b182622ca8b945a4f56f496f810f7ba5
Duane Felton <dfelton2010@gmail.com>
2018-02-27 20:16:23 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var index = require("./index.js")
-var name = ("Susan")
-var height = 
+var name = "Susan"
+var height = "70"
+
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
